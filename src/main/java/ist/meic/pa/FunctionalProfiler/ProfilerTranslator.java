32faaedb0a13e507b26d5beb423d33cf7a04e973
jplmoreira <jplmoreira92@gmail.com>
2019-03-23 12:11:30 +0000

    Add constructor profiling to the translator

--- src/main/java/ist/meic/pa/FunctionalProfiler/ProfilerTranslator.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/ProfilerTranslator.java
@@ -2,6 +2,7 @@ package ist.meic.pa.FunctionalProfiler;
 
 import javassist.CannotCompileException;
 import javassist.NotFoundException;
+import javassist.CtConstructor;
 import javassist.Translator;
 import javassist.ClassPool;
 import javassist.CtMethod;
@@ -16,6 +17,35 @@ public class ProfilerTranslator implements Translator {
     private final String readerTemplate = "ist.meic.pa.FunctionalProfiler.ProfilerMaps.incrementReader(\"%s\");" +
                                           "$_ = $proceed();";
 
+    private ExprEditor methodEditor = new ExprEditor() {
+            public void edit(FieldAccess fa) throws CannotCompileException {
+                String className = fa.getClassName();
+                if (!className.equals("java.lang.System") &&
+                    !className.equals("ist.meic.pa.FunctionalProfiler.ProfilerMaps")) {
+                    if (fa.isWriter()) {
+                        fa.replace(String.format(writerTemplate, className));
+                    } else if (fa.isReader()) {
+                        fa.replace(String.format(readerTemplate, className));
+                    }
+                }
+            }
+        };
+
+    private ExprEditor constEditor = new ExprEditor() {
+            public void edit(FieldAccess fa) throws CannotCompileException {
+                String className = fa.getClassName();
+                boolean initialization = className.equals(fa.where().getDeclaringClass().getName());
+                if (!className.equals("java.lang.System") &&
+                    !className.equals("ist.meic.pa.FunctionalProfiler.ProfilerMaps")) {
+                    if (fa.isWriter() && !initialization) {
+                        fa.replace(String.format(writerTemplate, className));
+                    } else if (fa.isReader()) {
+                        fa.replace(String.format(readerTemplate, className));
+                    }
+                }
+            }
+        };
+
     public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
         // Do nothing
     }
@@ -26,21 +56,10 @@ public class ProfilerTranslator implements Translator {
     }
 
     private void profileClass(CtClass ctClass) throws CannotCompileException {
-        for (CtMethod ctMethod: ctClass.getDeclaredMethods()){
-            ctMethod.instrument(new ExprEditor() {
-                    public void edit(FieldAccess fa)
-                        throws CannotCompileException {
-                        String className = fa.getClassName();
-                        if (!className.equals("java.lang.System") &&
-                            !className.equals("ist.meic.pa.FunctionalProfiler.ProfilerMaps")) {
-                            if (fa.isWriter()) {
-                                fa.replace(String.format(writerTemplate, className));
-                            } else if (fa.isReader()) {
-                                fa.replace(String.format(readerTemplate, className));
-                            }
-                        }
-                    }
-                });
-        }
+        for (CtMethod ctMethod: ctClass.getDeclaredMethods())
+            ctMethod.instrument(methodEditor);
+
+        for (CtConstructor ctConstructor: ctClass.getDeclaredConstructors())
+            ctConstructor.instrument(constEditor);
     }
 }
